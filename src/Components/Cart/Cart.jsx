769fef801982d6f6f4ac75e4205bf1dc0efc185d
OmerShaysh442 <omershaysh@gmail.com>
2021-05-24 17:47:26 +0300

    created checkout form

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -11,12 +11,12 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
   const handleEmptyCart = () => onEmptyCart();
 
   const renderEmptyCart = () => (
-    <Typography variant="subtitle1">You have no items in your milling cart,
-      <Link className={classes.link} to="/">start adding some</Link>!
+    <Typography variant="subtitle1">The flour mill is empty,
+      <Link className={classes.link} to="/">would you like to choose some seeds to grind</Link>?
     </Typography>
   );
 
-  if (!cart.line_items) return 'Loading';
+  if (!cart.line_items) return '...Loading';
 
   const renderCart = () => (
     <>
@@ -29,10 +29,10 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
       </Grid>
       <div className={classes.cardDetails}>
         <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
-        <div>
+        <Typography gutterBottom variant="h5" component="h2">
           <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
           <Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" color="primary">Checkout</Button>
-        </div>
+        </Typography>
       </div>
     </>
   );
@@ -40,7 +40,7 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3" gutterBottom>Your Milling Cart</Typography>
+      <Typography className={classes.title} variant="h3" gutterBottom>Your Flour Blend</Typography>
       { !cart.line_items.length ? renderEmptyCart() : renderCart() }
     </Container>
   );
