d40235567e8a6782dd7eba2810c4630711cf1ec9
OmerShaysh442 <omershaysh@gmail.com>
2021-05-24 12:47:14 +0300

    created cart folder for fech the products

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id:1, name: 'Wheat', description: 'Manitova', price: '$5', image: 'https://www.wellsafe.in/wp-content/uploads/2020/10/Wheat.jpg'},
-    { id:2, name: 'Spelt', description: 'Spring', price: '$10', image: 'https://www.vacqpack.com/wp-content/uploads/sites/3/2019/11/wheat-1-768x350.jpg'},
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
     return (
     <main className={classes.content}>
@@ -17,7 +12,7 @@ const Products = () => {
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product}/>
+                    <Product product={product} onAddToCart={onAddToCart} />
                 </Grid> 
             ))}
         </Grid>
